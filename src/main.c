9b80b948ca1a448f4024d193d28f25e5b31a87ca
Maxence MICHOT <maxence@michot.re>
2026-02-01 02:34:32 +0100

    fix: remove unused imports

--- src/main.c
+++ src/main.c
@@ -1,8 +1,5 @@
 // #define WALLPAPER
 //  SDL3
-#include <SDL3/SDL_init.h>
-#include <SDL3/SDL_properties.h>
-#include <SDL3/SDL_video.h>
 #define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
